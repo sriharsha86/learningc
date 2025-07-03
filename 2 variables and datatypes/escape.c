93e72e88228b68777a72634c760be6a866697a23
Harsha <hk.harshapshv@gmail.com>
2025-07-03 20:32:05 +0530

    2

---
+++ 2 variables and datatypes/escape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+printf("Harsha \n Vardan");
+printf("Harsha \t Vardan");
+printf("Harsha \b Vardan");
+printf("Harsha \" Vardan\"");
+return 0;
+}
---
+++ 2 variables and datatypes/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int a = 5;
+    const PI = 3.14;
+    
+    printf("%d\n", a );
+
+    a = 6;
+
+    printf("%d", a);
+
+
+    return 0;
+}
